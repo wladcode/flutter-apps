482048cd3e7e6a20844d30dd2c63301901ae8062
Wladimir Lopez <wlopez@ebos.ec>
2026-06-10 21:09:03 -0500

    rutas con nombre

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -22,23 +23,21 @@ class _HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: appMenuItems.length,
-      itemBuilder: (context, index){
-      final menuItem = appMenuItems[index];
-      return _CustomListTile(menuItem: menuItem);
-    });
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
   }
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({
-    required this.menuItem,
-  });
+  const _CustomListTile({required this.menuItem});
 
   final MenuItem menuItem;
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
     return ListTile(
       title: Text(menuItem.title),
@@ -46,16 +45,24 @@ class _CustomListTile extends StatelessWidget {
       leading: Icon(menuItem.icon, color: colors.primary),
       trailing: Icon(Icons.arrow_forward_ios, color: colors.primary),
       onTap: () {
-        /*
-Navigator.of(context).push(
-    MaterialPageRoute<void>(
-      builder: (context) => const ButtonsScreen(),
-    ),
-  );      
-  */
-  //Navigator.pushNamed(context, menuItem.link);
-  context.push(menuItem.link);
-  },
+        // Para navegacion con Navigator.of(context).push
+              /*
+      Navigator.of(context).push(
+          MaterialPageRoute<void>(
+            builder: (context) => const ButtonsScreen(),
+          ),
+        );      
+        */  
+
+        // Para navegacion con Navigator.pushNamed
+        //Navigator.pushNamed(context, menuItem.link);
+
+        // Para navegacion con GoRouter
+        context.push(menuItem.link);
+        
+        // Para navegacion con GoRouter por nombre de la ruta
+        //context.pushNamed(ButtonsScreen.name);
+      },
     );
   }
 }
